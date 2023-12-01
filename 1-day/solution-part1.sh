55bc81cc070c431f0ee409c43104e6246bc4a2be
Adam Celmer <adam.celmer@unblu.com>
2023-12-01 18:21:45 +0100

    day 1

---
+++ 1-day/solution-part1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sum=0
+while read -r line; do
+  only_nums="$(echo $line | sed 's/[a-zA-Z]//g')"
+  num_first=${only_nums:0:1}
+  num_last=${only_nums: -1}
+  num="${num_first}${num_last}"
+  sum=$(($sum + $num))
+done < input.txt
+
+echo "sum: ${sum}"
